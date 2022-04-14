8fe467bfa2f021603bc400ac08ef7e156bb65200
rlatm <rlatmdwp5@gmail.com>
2022-04-15 06:14:29 +0900

    ThreadProgramming

---
+++ 56_Thread/UnitTest/ThreadDemo.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "Systems/IExecute.h"
+
+class ThreadDemo : public IExecute
+{
+public:
+	// Inherited via IExecute
+	virtual void Initialize() override;
+	virtual void Ready() override {}
+	virtual void Destroy() override {}
+	virtual void Update() override;
+	virtual void PreRender() override {}
+	virtual void Render() override;
+	virtual void PostRender() override {}
+	virtual void ResizeScreen() override {}
+
+private:
+	void Loop();
+
+	void Function();
+	
+	void MultiThread();
+	void MultiThread1();
+	void MultiThread2();
+
+	void Join();
+};
+
